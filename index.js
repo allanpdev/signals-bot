515d6a10baccca02bf7030a2c178756969f1813b
Allan <109388058+allanpdev@users.noreply.github.com>
2025-05-09 11:01:40 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const TelegramBot = require('node-telegram-bot-api')
+const Binance = require('node-binance-api')
+
+const binance = new Binance().options({
+    APIKEY: 'ixprfcTkudGYjwGDVtuYDiQqFv79H0DQXyV09iMKqf2SDy5jaRAUY7hAc6qdMO4Q',
+    APISECRET: 'o3rAu3O3jn4icCXGSYVRDGynhYX5Q1ow1zt3KbbOZaaATb0vFfYRgqtYSZ8hxcGq'
+})
+
+binance.prices('BTCUSDT', (error, ticker) => {
+    if(error){
+        console.error('Erro ao consultar preço: ', error.body)
+    } else{
+        console.log('Preço atual: ', ticker.BTCUSDT)
+    }
+})
 
 const token = '7873617722:AAG7yZ9iJt7cgITJTM0iHnvSxwvZOi_lCGU'
 
